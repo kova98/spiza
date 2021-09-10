8f5167f34b20683ca5805fd1be411c5e822775d2
Roko <rokokovac98@gmail.com>
2021-09-10 21:21:40 +0200

    Add Restaurant service

---
+++ Spiza/Services/Spiza.Services.Restaurant/RestaurantService.cs
@@ -0,0 +1,19 @@
+﻿
+using Grpc.Core;
+using Spiza.Services.Restaurant.Repositories;
+
+namespace Spiza.Services.Restaurant;
+public class RestaurantService : Restaurant.RestaurantBase
+{
+    private readonly IRestaurantsRepository restaurantsRepo;
+
+    public RestaurantService(IRestaurantsRepository restaurantsRepo)
+    {
+        this.restaurantsRepo = restaurantsRepo;
+    }
+
+    public override Task<RestaurantsResponse> GetRestaurants(RestaurantParameters request, ServerCallContext context)
+    {
+        return base.GetRestaurants(request, context);
+    }
+}
