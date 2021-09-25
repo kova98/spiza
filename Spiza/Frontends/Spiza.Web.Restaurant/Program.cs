8d8127dea8fabcb178d137c09186fe630720b637
Roko <rokokovac98@gmail.com>
2021-09-25 20:54:21 +0200

    Add MudBlazor

--- Spiza/Frontends/Spiza.Web.Restaurant/Program.cs
+++ Spiza/Frontends/Spiza.Web.Restaurant/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Spiza.Web.Restaurant;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
