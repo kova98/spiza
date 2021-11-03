9f269323716652f2ba50712cae6abc331762100c
Roko <rokokovac98@gmail.com>
2021-11-03 23:06:07 +0100

    Add menu screen

---
+++ Spiza/Frontends/Spiza.Mobile.Customer/spiza_customer/lib/screens/menu_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:spiza_customer/models/restaurant.dart';
+import 'package:spiza_customer/widgets/menu_list.dart';
+
+class MenuScreen extends StatefulWidget {
+  final Restaurant restaurant;
+
+  const MenuScreen({Key key, this.restaurant}) : super(key: key);
+
+  @override
+  _MenuScreenState createState() => _MenuScreenState(restaurant);
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  final Restaurant restaurant;
+  _MenuScreenState(this.restaurant);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.transparent,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  image: AssetImage("assets/burger.png"),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      '20-30 min',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        restaurant.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                        ),
+                      ),
+                      Text(
+                        '★ 4.5',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Delivery 5,00 kn',
+                    style: TextStyle(fontSize: 18, color: Colors.black87),
+                  ),
+                  SizedBox(height: 15),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Spiza/Frontends/Spiza.Mobile.Customer/spiza_customer/lib/widgets/restaurants_list.dart
+++ Spiza/Frontends/Spiza.Mobile.Customer/spiza_customer/lib/widgets/restaurants_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spiza_customer/bloc/restaurants_provider.dart';
 import 'package:spiza_customer/models/restaurant.dart';
+import 'package:spiza_customer/screens/menu_screen.dart';
 
 class RestaurantsList extends StatelessWidget {
   Widget build(context) {
@@ -20,33 +21,41 @@ class RestaurantsList extends StatelessWidget {
               itemCount: snapshot.data.length,
               itemBuilder: (context, int index) {
                 final item = snapshot.data[index];
-                return Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image(image: AssetImage("assets/burger.png")),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            item.name,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                return InkWell(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MenuScreen(restaurant: item),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image(image: AssetImage("assets/burger.png")),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              item.name,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '4.5',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              '★ 4.5',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Text('5,00 kn')
-                    ],
+                          ],
+                        ),
+                        Text('5,00 kn')
+                      ],
+                    ),
                   ),
                 );
               },
