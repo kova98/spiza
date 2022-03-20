426b61f75bde9a1f78ee809803e760894f44109f
Roko <rokokovac98@gmail.com>
2022-03-20 22:37:33 +0100

    Fix empty category or item throwing exception

--- src/frontends/spiza_customer/lib/models/menu.dart
+++ src/frontends/spiza_customer/lib/models/menu.dart
@@ -5,10 +5,10 @@ class Menu {
   final List<Item> items;
 
   Menu.fromJson(Map<String, dynamic> parsedJson)
-      : categories = parsedJson != null
+      : categories = parsedJson["categories"] != null
             ? (parsedJson['categories'] as List).cast<String>()
             : List<String>(),
-        items = parsedJson != null
+        items = parsedJson['items'] != null
             ? (parsedJson['items'] as List)
                 .map<Item>((e) => Item.fromJson(e))
                 .toList()
