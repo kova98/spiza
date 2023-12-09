6aab8415d6148a4e022bc74db4d8913251d68214
Roko <rokokovac98@gmail.com>
2023-12-09 13:59:56 +0100

    Init db on startup

--- src/services/api/data/db.go
+++ src/services/api/data/db.go
@@ -14,31 +14,32 @@ func InitDb(connStr string) *sql.DB {
 		log.Fatal(err)
 	}
 
+	init := `
+        CREATE TABLE IF NOT EXISTS restaurants (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS menu_categories (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            restaurant_id INTEGER REFERENCES restaurants(id)
+        );
+        
+        CREATE TABLE IF NOT EXISTS items (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            category_id INTEGER REFERENCES menu_categories(id),
+            order_num INTEGER,
+            price NUMERIC,
+            description TEXT,
+            image TEXT
+        );
+        `
+	_, err = db.Exec(init)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return db
 }
-
-/*
-
-
-CREATE TABLE restaurants (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
-);
-
-CREATE TABLE menu_categories (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    restaurant_id INTEGER REFERENCES restaurants(id)
-);
-
-CREATE TABLE items (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    category_id INTEGER REFERENCES menu_categories(id),
-    order_num INTEGER,
-    price NUMERIC,
-    description TEXT,
-    image TEXT
-);
-
-*/
