7884404117347fdbad104e8c58f565ba176332ff
Roko <rokokovac98@gmail.com>
2024-01-31 17:31:50 +0100

    Add monitor service

---
+++ src/services/monitor/go.mod
@@ -0,0 +1,3 @@
+module github.com/kova98/spiza/services/monitor
+
+go 1.21.4
---
+++ src/services/monitor/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	l := log.New(os.Stdout, "monitor-", log.LstdFlags)
+	l.Println("Starting monitor")
+	connStr := os.Getenv("SPIZA_DB_CONN_STR")
+	if connStr == "" {
+		l.Fatal("SPIZA_DB_CONN_STR environment variable empty")
+	}
+	googleApiKey := os.Getenv("GOOGLE_API_KEY")
+	if googleApiKey == "" {
+		l.Fatal("GOOGLE_API_KEY environment variable empty")
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	sig := <-c
+	log.Println("Got signal:", sig)
+}
