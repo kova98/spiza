7ab484a913205f6b1575e22c4a31c94e2c0d757d
Roko <rokokovac98@gmail.com>
2024-02-08 22:14:29 +0100

    Fix order card layout on small screens

--- src/frontends/web/spiza-restaurant/src/pages/Orders.svelte
+++ src/frontends/web/spiza-restaurant/src/pages/Orders.svelte
@@ -63,7 +63,7 @@
 </script>
 
 <h1 class="font-bold text-4xl mb-5">Orders</h1>
-<div class="flex flex-row justify-between gap-3">
+<div class="flex flex-col sm:flex-row justify-between gap-3">
   <div class="w-full flex flex-col gap-3 items-center">
     {#each orders ?? [] as order}
       <OrderCard {order} {updateOrder} />
