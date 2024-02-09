84e19e45f7d3ee80bcb01f0186a4faf5485db452
Roko <rokokovac98@gmail.com>
2024-02-09 15:34:24 +0100

    Update navbar styling

--- src/frontends/web/spiza-restaurant/src/pages/Restaurant.svelte
+++ src/frontends/web/spiza-restaurant/src/pages/Restaurant.svelte
@@ -3,15 +3,33 @@
   import { cn } from "../lib/utils";
   import { Exit } from "radix-icons-svelte";
   import { restaurantStore } from "../lib/stores";
-  import { links } from "svelte-routing";
+  import { links, useLocation } from "svelte-routing";
+  let location = useLocation();
+
+  let paths = [
+    { path: "/restaurant", name: "Overview", active: false },
+    { path: "/restaurant/menu", name: "Menu", active: false },
+    { path: "/restaurant/orders", name: "Orders", active: false },
+  ];
+
+  location.subscribe((value) => {
+    paths.forEach((path) => {
+      path.active = value.pathname === path.path;
+    });
+  });
 </script>
 
 <nav class={cn("flex flex-row items-center space-x-6 p-6 shadow-sm")} use:links>
-  <h1 class="font-bold text-xl">{$restaurantStore.name}</h1>
-  <a href="/restaurant" class="text-sm font-medium transition-colors hover:text-primary"> Overview </a>
+  <h1 class="font-extrabold text-xl">{$restaurantStore.name}</h1>
 
-  <a href="/restaurant/menu" class="text-sm font-medium transition-colors hover:text-primary"> Menu </a>
-  <a href="/restaurant/orders" class="text-sm font-medium transition-colors hover:text-primary"> Orders </a>
+  {#each paths as path}
+    <a
+      href={path.path}
+      class="{path.active ? 'text-black' : 'text-gray-400'} font-medium text-sm transition-colors hover:text-"
+      >{path.name}</a
+    >
+  {/each}
+  <div class="flex-grow" />
   <Button class="ml-auto" variant="ghost" href="/"><Exit /></Button>
 </nav>
 <div class="p-6">
